6dbe7a7884ba4d4cec90ce6fa2af2860cc75c508
thiago.vicente@ua.pt <thiago.vicente@ua.pt>
2024-10-18 16:49:51 +0100

    joaoburro

--- backup_files.sh
+++ backup_files.sh
@@ -1,7 +1,7 @@
+
 #!/bin/bash
 
-. ./copyFile.sh
-source copyFile.sh
+# João Pereira[] & Thiago Vicente[121497]
 
 # Initializing variables
 checking=""
@@ -16,6 +16,8 @@ cWarnings=0;
 cUpdated=0;
 cCopied=0;
 cDeleted=0;
+
+# Initializing sizes
 sizeCopied=0;
 sizeDeleted=0;
 
@@ -34,9 +36,69 @@ nfound(){
 	exit 1
 }
 
-#compModDate(){
-	# Check wich file was first created and increment/print warning 
-#}
+compModDate(){
+	return 0;	
+}
+
+# after validation does backup. EXPECTS VALIDATED INPUT
+backup(){
+    	#function has 3 arguments
+    	#argument1 : absolute path of file to be copied
+   	#argument2 : absolute path of directory where file will be copied
+    	#argument3 : -c 
+    	
+
+    if [[ $# == 2 ]]; then
+      local file=$1
+      local destination_true=$2
+	
+	local filename=$(basename "$file")
+      local destination="$backup_dir/$filename"
+      
+      if [[ -e $destination ]]; then
+          compModDate "$file" "$destination"
+	    if [[ $? -ne 0 ]];then
+	    	return 0
+	    fi
+      fi
+
+	cp -a $file "$destination_true"
+    
+        if [[ $? -eq 0 ]]; then
+            
+            echo "cp -a $file $destination_true"
+
+            echo "" #prints new line
+            return 0
+        else
+            echo "Error while copying"
+        
+            echo "" #prints new line
+            return 1
+        fi
+
+    elif [[ $# == 3 ]]; then
+        local file=$2
+        local destination=$3
+        
+	  
+	local filename=$(basename "$file")
+      local destination_true="$backup_dir/$filename"
+      
+      if [[ -e $destination ]]; then
+          compModDate "$file" "$destination"
+	    if [[ $? -ne 0 ]];then
+	    	continue
+	    fi
+      fi
+
+        echo "cp -a $file $destination_true"
+        echo ""
+
+        return 0
+    fi
+}
+
 
 ## MAIN
 
@@ -45,6 +107,7 @@ while getopts ":cb:r:" op; do
     case $op in
 	c)
             echo "checking activated"
+		checking=1
 	;;
 	*)
 		usage
@@ -57,13 +120,8 @@ source_dir="$1"
 backup_dir="$2"
 
 # validate source directory
-if [ -d $source_dir ]; then
-	if [ "$checking" == "1" ];then
-		echo "Source directory exists"
-	fi
-else
+if [ ! -d $source_dir ]; then
 	nfound "source" "$source_dir"
-
 fi
 
 # backup directory
@@ -78,7 +136,11 @@ fi
 for file in "$source_dir"/*; do
     	
 	if [[ -e $file ]]; then  # make sure
-		copyFile "$file" "$backup_dir"
+		if [ "$checking" == "1" ];then
+			backup 1 "$file" "$backup_dir"
+		else
+			backup "$file" "$backup_dir"
+		fi
 	fi
 
 done
