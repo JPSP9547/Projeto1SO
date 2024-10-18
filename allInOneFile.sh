f7d9ecb7c8ca3775d4c9df18d9cebba7450cd625
JPSP9547 <joaopp@ua.pt>
2024-10-18 21:12:33 +0100

    copyFile Function done

--- allInOneFile.sh
+++ allInOneFile.sh
@@ -23,6 +23,71 @@ compModDate(){
 	
 }
 
+copyFile(){
+    	#function has 3 arguments
+    	#argument1 : absolute path of file to be copied
+     	#argument2 : absolute path of directory where file will be copied
+    	#argument3 : -c
+        #function checks dates of files and only copies if source file is newer than backed files
+        #FUNCTION ASSUMES ARGUMENT1 IS NOT A DIRECTORY
+
+        if [[ $# == 2 ]]; then
+            
+            file=$1
+            destination=$2
+            copy=1
+        elif [[ $# == 3 ]]; then
+            
+            file=$2
+            destination=$3
+            copy=0
+
+        else
+            echo "Bad use of function copyFile"
+        fi
+
+
+        fileName="$(echo $file | tr "/" " " | awk '{print $NF}')"
+
+        if [[ $destination == */  ]]; then
+            destinationFormat="${destination%?}"        
+        fi
+
+    
+        if [[ -f "$destinationFormat/$fileName" ]]; then
+            compModDate $file "$destination/$fileName"
+
+            if [[ $? -eq 0 ]]; then
+
+                if [[ $copy -eq 1 ]]; then
+                    cp -a $file $destination
+                    return 0
+                fi
+
+                echo "cp -a $file $destinationFormat/$fileName"
+                return 0
+            else
+
+                echo "[Warning] BackedUp file newer than file in directory (Should not happen)"
+                return 1
+
+            fi
+
+        else
+
+            if [[ $copy -eq 1 ]]; then
+                cp -a $file $destination
+            fi
+
+            echo "cp -a $file $destinationFormat/$fileName"
+
+            return 0
+
+        fi
+}
+
+
+
 
 ######### Variabes
 
