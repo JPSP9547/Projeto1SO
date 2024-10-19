d4cd1f97753c9671e00b5a9eca73cb909576868a
thiago.vicente@ua.pt <thiago.vicente@ua.pt>
2024-10-19 03:29:14 +0100

    Ex2 file ready

---
+++ backup.sh
@@ -0,0 +1,256 @@
+#!/bin/bash
+
+######### Variables
+
+checking=""
+filter=""
+exclude_file=""
+source_dir=""
+backup_dir=""
+is_recursive=0
+
+# Initializing counter
+cError=0
+cWarnings=0
+cUpdated=0
+cCopied=0
+cDeleted=0
+
+# Initializing sizes
+sizeCopied=0
+sizeDeleted=0
+
+########### 
+nfound(){
+	# Prints not found message
+	# arg1 is the field name
+	# arg2 is the given path
+	if [[ "$is_recursive" -eq 0 ]];then
+		echo "[NOTFOUND]: "$1 ">" $2
+	fi
+	end_print
+}
+
+usage(){
+	if [[ "$is_recursive" -eq 0 ]];then
+		echo "[USAGE] ./backup.sh [-c] dir_source dir_backup"
+	fi
+	end_print 1
+}
+end_print(){
+	if [[ "$is_recursive" -eq 1 ]];then
+		local result=("$cError" "$cWarnings" "$cUpdated" "$cCopied" "$sizeCopied" "$cDeleted" "$sizeDeleted")
+		echo "${result[@]}"
+		exit 0
+	else
+		echo "While backing up src: $cError Errors; $cWarnings Warnings; $cUpdated Updated; $cCopied Copied (${sizeCopied}B); $cDeleted deleted (${sizeDeleted}B)"
+		exit $1
+	fi
+}
+compModDate(){
+	# Functions has two parameters
+	# arg1 is the source_dir file
+	# arg2 is the backup_dir file
+	# RETURN: returns 0 if arg1 last modification was after arg2 last modification
+	# RETURN: return 1 otherwise
+	# FUNCTIONS ASSUMES THAT ARG1 and ARG2 exists
+
+	local file1="$1"
+	local file2="$2"
+
+	if [[ "$file1" -nt "$file2" ]];then
+		return 0	
+    elif [[ "$file2" -nt "$file1" ]];then
+        echo "[WARNING] Backed file ($file2) is newer than source file ($file1) (SHOULD NOT HAPPEN)"
+	  	((cWarnings++))
+        return 1
+    else
+        return 1 #if dates are equal it will not copy the file
+    fi	
+}
+
+copyFile(){
+    	#function has 3 arguments
+    	#argument1 : absolute path of file to be copied
+     	#argument2 : absolute path of directory where file will be copied
+    	#argument3 : -c
+        #function checks dates of files and only copies if source file is newer than backed files
+        #FUNCTION ASSUMES ARGUMENT1 IS NOT A DIRECTORY
+
+        if [[ $# -eq 2 ]]; then
+            file=$1
+            destination=$2
+            copy=1
+        elif [[ $# -eq 3 ]]; then
+            
+            file=$2
+            destination=$3
+            copy=0
+
+        else
+            echo "Bad use of function copyFile"
+			return 1
+        fi
+
+	  	file="$(echo $file | tr -s '/')"
+        fileName="$(basename "$file")"
+        
+        if [[ -f "$destination/$fileName" ]]; then
+            compModDate "$file" "$destination/$fileName"
+
+            if [[ $? -eq 0 ]]; then
+
+                if [[ $copy -eq 1 ]]; then
+                    cp -a "$file" "$destination"
+					file_size=$(stat -c %s "$file")
+    				((cCopied++))
+					sizeCopied=$((sizeCopied + file_size))
+                    return 0
+                fi
+
+                echo "cp -a $file $destination/$fileName"
+	
+		    	file_size=$(stat -c %s "$file")
+    			((cUpdated++))
+				sizeCopied=$((sizeCopied + file_size))
+		    	return 0
+
+			
+	    	else
+                return 1
+
+            fi
+
+        else
+
+        if [[ $copy -eq 1 ]]; then
+            cp -a "$file" "$destination"
+         fi
+
+        echo "cp -a $file $destination/$fileName"
+        ((cCopied++))
+
+		file_size=$(stat -c %s "$file")
+		sizeCopied=$((sizeCopied + file_size))
+        return 0
+    fi
+}
+
+
+########### MAIN
+
+# save the name for recursive call
+function_call="$0" 
+
+
+# get options/flags
+while getopts ":cz" op; do
+    case $op in
+	c)
+        echo "Checking activated"
+		checking=1
+	;;
+	z)
+		is_recursive=1
+	;;
+	*)		
+		usage 1
+	;;
+    esac
+done
+
+shift $((OPTIND-1))
+if [[ $# -ne 2 ]]; then
+	usage 1
+fi
+
+source_dir="$1"
+backup_dir="$2"
+
+
+if [[ "$source_dir" != /* &&  "$source_dir" != ./* ]]; then
+	source_dir="./$source_dir"
+fi
+if [[ "$backup_dir" != /* && "$backup_dir" != ./* ]]; then
+    backup_dir="./$backup_dir"
+fi
+# removes last bar(/) from backup_dir path (for formatting reasons)
+if [[ $source_dir == */ ]]; then
+    source_dir="${source_dir:0:-1}" 
+fi
+
+if [[ $backup_dir == */ ]]; then
+    backup_dir="${backup_dir:0:-1}"
+fi
+
+# validate source directory
+if [ ! -d "$source_dir" ]; then
+	nfound "source" "$source_dir"
+fi
+
+# backup directory
+if [ ! -d "$backup_dir" ];then
+	if [ -z "$checking" ];then
+		mkdir -p "$backup_dir"
+	fi
+	echo "mkdir $backup_dir"
+fi
+
+
+## start backup of the source files
+# call copy for each source file
+for item in "$source_dir"/*; do
+
+	if [[ -f $item ]]; then 
+		if [ "$checking" == "1" ];then
+			copyFile 1 "$item" "$backup_dir"
+		else
+			copyFile "$item" "$backup_dir"
+		fi
+	elif [[ -d $item ]];then
+		new_backup_dir="$backup_dir/$(basename "$item")"
+		
+		if [[ "$checking" -eq 1 ]]; then
+    		command="$function_call -c -z $item $new_backup_dir"
+		else
+    		command="$function_call -z $item $new_backup_dir"
+		fi
+
+		#echo $command
+		output="$($command )"
+		echo "$output" | grep -E '^(cp|mkdir)' 
+
+		last_line="${output##*$'\n'}"
+		res=($last_line)
+		
+		cError=$((cError + res[0]))
+		cWarnings=$((cWarnings + res[1]))
+		cUpdated=$((cUpdated + res[2]))
+		cCopied=$((cCopied + res[3]))
+		sizeCopied=$((sizeCopied + res[4]))
+		cDeleted=$((cDeleted + res[5]))
+		sizeDeleted=$((sizeDeleted + res[6]))
+
+	fi
+
+done
+
+## Remove files from backup_dir that are not on source_dir
+# Skip when backup dir is empty 
+if [[ -d "$backup_dir" &&  ! -z "$(ls -A "$backup_dir")" ]]; then 
+	for file in "$backup_dir"/*; do
+    	filename=$(basename "$file")
+    	if [[ ! -e "$source_dir/$filename" ]]; then
+     
+			file_size=$(stat -c %s "$file")
+			sizeDeleted=$((sizeDeleted + file_size))
+			if [ -z "$checking" ];then
+	    			rm "$file"
+			fi
+	    	((cDeleted++))
+    	fi
+	done
+fi
+
+end_print 0
+orrect it pls
